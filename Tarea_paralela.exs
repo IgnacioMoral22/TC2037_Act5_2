ccd4a49ed4ffce15e7021ccea8d60a49cf5cebdb
Ignacio Joaquin Moral <71529339+IgnacioMoral22@users.noreply.github.com>
2022-06-14 13:34:50 -0500

    Add files via upload

--- Tarea_paralela.exs
+++ Tarea_paralela.exs
@@ -1,33 +1,35 @@
 defmodule Hw.Primes do
 
-  def check_prime(0, _number), do: false
-  def check_prime(1, _number), do: false
-  def check_prime(_dividend, number) when number == 2, do: false
-  def check_prime(dividend, number), do: rem(number, dividend) == 0
-
-  def get_prime_sum(0), do: 0
-  def get_prime_sum({start, finish}), do: do_get_prime_sum(start, finish, 0)
-  def get_prime_sum(limit), do: do_get_prime_sum(0, limit, 0)
-  defp do_get_prime_sum(start, finish, result) when start>finish, do: result
-  defp do_get_prime_sum(start, finish, result) when start<=finish do
-    root = trunc(:math.sqrt(start))
-    prime_check = 2..root
-    |> Enum.to_list()
-    |> Enum.map(&check_prime(&1, start))
-    |> Enum.member?(true)
-
-    if(prime_check) do
-      do_get_prime_sum(start+1, finish, result)
+  def check_prime(0, _number, _root), do: false
+  def check_prime(1, _number, _root), do: false
+  def check_prime(_dividend, number, _root) when number == 2, do: false
+  def check_prime(dividend, _number, root) when dividend>root, do: false
+  def check_prime(dividend, number, root) when dividend<=root do
+    if rem(number, dividend) == 0 do
+      true
     else
-      do_get_prime_sum(start+1, finish, result+start)
+      check_prime(dividend+1, number, root)
     end
   end
 
-  def get_prime_parallel(limit, threads \\ System.schedulers) do
+  def sum_primes(0), do: 0
+  def sum_primes({start, finish}), do: do_sum_primes(start, finish, 0)
+  def sum_primes(limit), do: do_sum_primes(0, limit, 0)
+  defp do_sum_primes(start, finish, result) when start>finish, do: result
+  defp do_sum_primes(start, finish, result) when start<=finish do
+    root = trunc(:math.sqrt(start))
+    if(check_prime(2, start, root)) do
+      do_sum_primes(start+1, finish, result)
+    else
+      do_sum_primes(start+1, finish, result+start)
+    end
+  end
+
+  def sum_primes_parallel(limit, threads \\ System.schedulers) do
     range = div(limit, threads)
     1..threads
     |> Enum.map(fn x -> {range*(x-1)+1, range*x} end)
-    |> Enum.map(&Task.async(fn -> get_prime_sum(&1) end))
+    |> Enum.map(&Task.async(fn -> sum_primes(&1) end))
     |> Enum.map(&Task.await(&1))
     |> Enum.sum()
 
